496c6e715bef79cc43f3d4343d1f02ee492adfb4
Alexander DeJesus <Alex@Alexanders-MacBook-Pro-2.local>
2018-01-18 12:56:22 -0500

    lunch revision

--- beal_katas/beginner_sushi_1.py
+++ beal_katas/beginner_sushi_1.py
@@ -2,4 +2,4 @@ import random
 name = input('What is your name?')
 my_number = input('Hello ' + name + ' What is your number?')
 my_number = int(my_number) + random.randint(1,100)
-print('Your hashed number is ' + str(my_number))
+print('Your edited number is ' + str(my_number))
