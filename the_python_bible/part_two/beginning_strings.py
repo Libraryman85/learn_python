43cd98fa1ee96c1e7fbd5acc797842f83d9d479f
Alexander DeJesus <Alex@Alexanders-MacBook-Pro-2.local>
2018-01-19 12:29:17 -0500

    python bible work

---
+++ the_python_bible/part_two/beginning_strings.py
@@ -0,0 +1,15 @@
+'hello'
+
+name = 'Ziyad'
+print(type(name))
+
+message = 'John said to me'
+
+romeo = """Two households, both alike in dignity, 
+in fair Verona, where we lay our scene"""
+
+print (romeo)
+
+hello = 'hello world!'
+
+print(hello)
