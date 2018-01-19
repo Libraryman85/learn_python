c78db545a0f3d00d627a3c4ffd9f4823ba90f20f
Alexander DeJesus <Alex@Alexanders-MacBook-Pro-2.local>
2018-01-19 12:40:18 -0500

    python bible work

---
+++ the_python_bible/part_two/hello_you_project.py
@@ -0,0 +1,13 @@
+# Ask user their name
+# Ask user for age
+# Ask user city
+# Ask user what they enjoy
+# create output text
+# print output to screen
+name = input('')
+
+age = input('')
+
+city = input('')
+
+enjoy = input('')
