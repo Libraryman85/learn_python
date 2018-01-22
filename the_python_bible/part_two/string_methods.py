3278c39e395ea2fa94cc28bb67c79c4c1ba0f8fc
libraryman85 <libraryman85@gmail.com>
2018-01-22 12:43:27 -0500

    python bible work

---
+++ the_python_bible/part_two/string_methods.py
@@ -0,0 +1,18 @@
+#using string method string.method()
+
+#'hello'.count('e')
+
+#text = 'Happy Birthday'
+#text.count('a')
+#text.count('day')
+
+
+x = 'Happy Birthday'
+
+x.lower()
+x.upper()
+x = x.upper()
+x = x.lower()
+
+
+
