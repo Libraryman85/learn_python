b336f45a63d5e1442630d291c85deec33c72e0e6
libraryman85 <libraryman85@gmail.com>
2018-01-22 12:36:18 -0500

    python bible work

---
+++ the_python_bible/part_two/tell_us_about_yourself_challenge.py
@@ -0,0 +1,27 @@
+# Create a variable called name and set it equal to your name
+
+
+# Create a variable called age and set it to your age in years
+# Note: This must be an int, not a string!
+
+
+# Create a variable called place and set it equal to where you live
+
+
+# Create a variable called output, and use string formatting to
+# make it like the example text in the description
+
+
+# Finally, use the print() function to print the output.
+
+name = 'Alex'
+
+age = 32
+
+place = 'Cleveland'
+
+string = 'Hello my name is {}, and I am {} years old. I live in {} and I love Python'
+
+output = string.format(name, age, place)
+
+print(output)
