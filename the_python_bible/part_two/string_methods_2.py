0b4c9d092901ee3a66dc85be082076ad71e05431
libraryman85 <libraryman85@gmail.com>
2018-01-23 22:56:44 -0500

    python bible work

---
+++ the_python_bible/part_two/string_methods_2.py
@@ -0,0 +1,7 @@
+x = 'happy birthday'
+x.index('birthday')
+#.index counts place where thing begins, beginning with 0
+
+x.find('birthday')
+#.find is used to stop program from erroring out, .index is used for hard stop if things go wrong
+
