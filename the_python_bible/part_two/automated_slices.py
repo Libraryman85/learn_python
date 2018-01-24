19c58d43fc66ecbadbe1de1c462bc40977b63fe6
libraryman85 <libraryman85@gmail.com>
2018-01-24 12:32:44 -0500

    python bible work

--- the_python_bible/part_two/automated_slices.py
+++ the_python_bible/part_two/automated_slices.py
@@ -1,4 +1,14 @@
 #can count from the end of a string, not just the beginning
 
 word = 'supercalifragilisticexpialidocious'
+
 print(word[-5])
+
+print(word[-1])
+
+print([word.index('cali')])
+
+print(word[word.index('docious'):])
+print(word[word.index('fragilistic'):word.index('exp')])
+
+#.index only returns first instance of word/phrase you are searching for 
