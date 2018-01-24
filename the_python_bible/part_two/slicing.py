3830f002bec597bb690e95990a8e84b39accc0be
libraryman85 <libraryman85@gmail.com>
2018-01-23 23:09:25 -0500

    python bible work

---
+++ the_python_bible/part_two/slicing.py
@@ -0,0 +1,8 @@
+string = 'ABCDEFG123456'
+#strings are iterable, can go step by step along it until you get to the end
+#each step is an element of the string, each element has a number
+#index #s start w/0
+word = 'supercalifragilisticexpialidocious'
+word[0]
+print(word[0])
+print(word[2])
