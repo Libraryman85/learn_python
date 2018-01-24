0554cf1cfe26f2ac444e65e6f3babb3748dd7756
libraryman85 <libraryman85@gmail.com>
2018-01-23 23:01:10 -0500

    python bible work

--- the_python_bible/part_two/string_methods_2.py
+++ the_python_bible/part_two/string_methods_2.py
@@ -1,7 +1,16 @@
 x = 'happy birthday'
 x.index('birthday')
-#.index counts place where thing begins, beginning with 0
+# .index counts place where thing begins, beginning with 0
 
 x.find('birthday')
-#.find is used to stop program from erroring out, .index is used for hard stop if things go wrong
+# .find is used to stop program from erroring out, .index is used for hard stop if things go wrong
 
+y = '0000000happybirthday0000000'
+
+y.strip('0')
+y.lstrip('0')
+y.rstrip('0')
+print(y.strip('0'))
+
+name = input('What is your name?: ')
+name = (name.strip())
