4784987f7c2812ed022cf98fb6298cfc91ff0366
libraryman85 <libraryman85@gmail.com>
2018-01-25 12:28:52 -0500

    Python 101 with Justin Beall

--- beal_katas/1_25_18.py
+++ beal_katas/1_25_18.py
@@ -1,15 +1,8 @@
-number_str = '123456789'
-
-def find_number(search):
-    if search in number_str:
-        print('hello')
-    else:
-        print('goodbye')
-
-find_number('0')
-
 #str = 'this is a str'
 #bool = True # False
 #int = 1
 #float = 1.0
 
+#if is a control structure
+if True:
+    print('hello world')
