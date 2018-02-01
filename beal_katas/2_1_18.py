8f1e0c83ea963d37f1403053a0814c80b1c444da
libraryman85 <libraryman85@gmail.com>
2018-02-01 12:22:23 -0500

    Python 101 with Justin Beall

---
+++ beal_katas/2_1_18.py
@@ -0,0 +1,15 @@
+#strings can be in single or double quotes
+#str = 'test'
+#str2 = "test"
+
+#string interpolation {}
+
+#bool
+#boolean  is true/false
+#bool = True
+#bool_false = False
+
+#int
+#int = 1
+#int = -1
+
