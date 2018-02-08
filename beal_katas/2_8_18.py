dc16344c1356d35e49b0c9c9a4daa9660fe997fa
libraryman85 <libraryman85@gmail.com>
2018-02-08 12:32:05 -0500

    Python 101 with Justin Beall

--- beal_katas/2_8_18.py
+++ beal_katas/2_8_18.py
@@ -1,3 +1,9 @@
 # casting changes data types
 # print each letter in a given string
 print('\n'.join('toast'))
+# .join sticks values together
+# createa a function that takes an nput, tehn prints each character of the input
+
+def print_characters(input_string):
+    for character in input_string:
+        print(character)
