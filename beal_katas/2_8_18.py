9a00312778893a6e965606ecd7ca219fda711bbe
libraryman85 <libraryman85@gmail.com>
2018-02-08 12:50:42 -0500

    reformatted code

--- beal_katas/2_8_18.py
+++ beal_katas/2_8_18.py
@@ -1,6 +1,8 @@
 # casting changes data types
 # print each letter in a given string
 print('\n'.join('toast'))
+
+
 # .join sticks values together
 # createa a function that takes an nput, tehn prints each character of the input
 
@@ -8,10 +10,13 @@ def print_characters(input_string):
     for character in input_string:
         print(character)
 
+
 # create a function tha ttakes 2 inputs, then prints true/false whether or ont the first
 # is contained within a second input
 
 text_value = 'some input'
+
+
 def search_string(search, text_input):
     result = False
     for character in text_input:
@@ -19,7 +24,7 @@ def search_string(search, text_input):
             result = True
     print(result)
 
+
 search_string('a', text_value)
 search_string('s', text_value)
 search_string('S', text_value)
-
