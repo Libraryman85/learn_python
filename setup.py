1631ba61d8d2a5b151393338e5f8a403b79280a2
libraryman85 <libraryman85@gmail.com>
2018-02-09 12:46:31 -0500

    Python 101 with Justin Beall

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='learn_python',
+    packages=find_packages(),
+    version='0.1',
+    description='A Python GitHub repository for practicing katas.',
+    author='Alexander DeJesus',
+    author_email='alexander.dejesus.1985@gmail.com',
+    url='https://github.com/libraryman85/learn_python',
+    keywords=['libraryman85', 'python', 'kata',],
+    install_requires=[
+        'pytest',
+    ],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+)
