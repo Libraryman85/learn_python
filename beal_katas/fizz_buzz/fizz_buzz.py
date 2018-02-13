d017f370b758871add883b314586d8051e6fd06d
libraryman85 <libraryman85@gmail.com>
2018-02-13 13:15:40 -0500

    Python 101 with Justin Beall

--- beal_katas/fizz_buzz/fizz_buzz.py
+++ beal_katas/fizz_buzz/fizz_buzz.py
@@ -2,10 +2,13 @@ def fizz_buzz(upper_bound):
     result = []
     for index in range(upper_bound):
         count = index + 1
-        if count % 3 == 0:
+        if count % 3 == 0 and count % 5 == 0:
+            result.append('fizzbuzz')
+        elif count % 3 == 0:
             result.append('fizz')
         elif count % 5 == 0:
             result.append('buzz')
+
         else:
 
 
