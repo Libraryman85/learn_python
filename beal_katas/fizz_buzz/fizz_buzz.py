cbd97b1bc7a4f76164e1a3cbb335b052d67f5cff
libraryman85 <libraryman85@gmail.com>
2018-02-13 12:54:30 -0500

    formatted code

--- beal_katas/fizz_buzz/fizz_buzz.py
+++ beal_katas/fizz_buzz/fizz_buzz.py
@@ -1,13 +1,10 @@
 def fizz_buzz(upper_bound):
     result = []
     for index in range(upper_bound):
-        count = index+1
-        if count %3 == 0:
+        count = index + 1
+        if count % 3 == 0:
             result.append('fizz')
         else:
-
-
-
 
             result.append(str(count))
     return result
