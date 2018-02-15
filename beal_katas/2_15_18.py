a08c1d0f05799592779466acf82907b5111a9860
libraryman85 <libraryman85@gmail.com>
2018-02-15 12:41:19 -0500

    python 101 exercise

--- beal_katas/2_15_18.py
+++ beal_katas/2_15_18.py
@@ -1,9 +1,23 @@
-print('----------------------------')
-print('          Hello App')
-print('----------------------------')
-print()
+# print('----------------------------')
+# print('          Hello App')
+# print('----------------------------')
+# print()
+#
+# name = input('What is your name? ')
+#
+# greeting = 'Nice to meet you ' + name
+# print(greeting)
 
-name = input('What is your name? ')
+# above is earlier exercise
 
-greeting = 'Nice to meet you ' + name
-print(greeting)
+# print character on a string
+
+grande = 'Batmon'
+
+count = 0
+
+for letter in grande:
+    count += 1
+    # count = count + 1
+    if count == 5:
+     print(letter)
