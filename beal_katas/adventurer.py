002a811a4fbda4e45e23d497a3c770aa868a2266
libraryman85 <libraryman85@gmail.com>
2018-02-15 12:53:03 -0500

    Practicing Classes

---
+++ beal_katas/adventurer.py
@@ -0,0 +1,15 @@
+class Adventurer:
+    def __init__(self, map):
+        self.map = map
+
+    pass
+
+
+good_map = 'good map'
+bad_map = 'bad map'
+
+alex = Adventurer(good_map)
+sarah = Adventurer(bad_map)
+grande = Adventurer(good_map)
+justin = Adventurer(bad_map)
+
