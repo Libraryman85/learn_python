339d426b1374e359572461774f17191659c38268
libraryman85 <libraryman85@gmail.com>
2018-02-15 12:20:53 -0500

    Creating 'Hello World' that asks user name

--- beal_katas/2_15_18.py
+++ beal_katas/2_15_18.py
@@ -1,4 +1,10 @@
 
-print('---------------------------')
+print('----------------------------')
 print('          Hello App')
 print('----------------------------')
+print()
+
+name = input('What is your name? ')
+
+greeting = 'Nice to meet you ' + name
+print(greeting)
