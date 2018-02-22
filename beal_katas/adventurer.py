8e04d94f08ba587d430b2040751ca931c23b75c9
libraryman85 <libraryman85@gmail.com>
2018-02-22 13:00:50 -0500

    Refining/adding to Adventurer.
    Defining 'Hat'

--- beal_katas/adventurer.py
+++ beal_katas/adventurer.py
@@ -1,37 +1,77 @@
+# class encapsulates behavior and staet
+
 import random
 
+
 class Adventurer:
-    def __init__(self, map, name, strength):
+    def __init__(self, map, name, equipment: dict):
         self.map = map
         self.name = name
-        self.strength = strength
+        self.hat = equipment['hat']
+        self.shoes = equipment['shoes']
 
-    def receive_map(self, other_adventurer):
-        other_map = other_adventurer.map
-        self.map = other_map
-
-
-def generate_character(map, name):
-    strength = random.randint(1, 18)
-    adventurer = Adventurer(map, name, strength)
-    return adventurer
+    def __str__(self):
+        return 'I am {self.name}. I wear {self.hat} hat and {self.shoes} shoes'
 
 
-good_map = 'good map'
-bad_map = 'bad map'
+class Hat:
+    def __init__(self, style: str):
+        self.style = style
 
-alex = generate_character(good_map, 'alex')
-sara = generate_character(bad_map, 'sara')
-grande = generate_character(good_map, 'grande')
-justin = generate_character(bad_map, 'justin')
-party = [alex, sara, grande, justin]
+    def a_method(self):
+        pass
 
-for adventurer in party:
-    print(adventurer.map + str(adventurer.strength))
+    def __str__(self):
+        return self.style
 
-sara.receive_map(grande)
 
-print(alex.map)
-print(sara.map)
-print(grande.map)
-print(justin.map)
+hat1 = Hat('beanie')
+hat2 = Hat('sports cap')
+hat3 = Hat('fedora')
+
+
+default_equipment = {
+    'hat' : Hat('peasant'),
+    'shoes' : None
+}
+sara = Adventurer('sara', default_equipment)
+
+    # def receive_map(self, other_adventurer):
+    #     other_map = other_adventurer.map
+    #     assert isinstance(other_map)
+    #     self.map = other_map
+    #
+
+
+
+
+hats = [hat1, hat2, hat3]
+
+for hat in hats:
+    print(hat)
+
+
+# def generate_character(map, name):
+#     strength = random.randint(1, 18)
+#     adventurer = Adventurer(map, name, strength)
+#     return adventurer
+#
+#
+# good_map = 'good map'
+# bad_map = 'bad map'
+#
+# alex = generate_character(good_map, 'alex')
+# sara = generate_character(bad_map, 'sara')
+# grande = generate_character(good_map, 'grande')
+# justin = generate_character(bad_map, 'justin')
+# party = [alex, sara, grande, justin]
+#
+# for adventurer in party:
+#     print(adventurer.map + str(adventurer.strength))
+#
+# sara.receive_map(grande)
+#
+# print(alex.map)
+# print(sara.map)
+# print(grande.map)
+# print(justin.map)
