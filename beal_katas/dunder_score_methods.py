def0827924e6c58d1bb40b9e5b883b5f1ee0e8c5
libraryman85 <libraryman85@gmail.com>
2018-03-06 12:29:23 -0500

    learnign dunder

---
+++ beal_katas/dunder_score_methods.py
@@ -0,0 +1,14 @@
+# dunder score methods
+
+# __add__ workds on the + operator
+# __eq__ works the = operator
+# __str__ works the stro operation
+# __init__ the method that is run when a new instance is created
+
+
+class Adventurer
+    def __init__(self):
+        pass
+
+
+sara = Adventurer()
