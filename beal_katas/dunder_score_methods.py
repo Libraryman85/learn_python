7c0105c9aa415db08f4dd292e3cb5d3179802579
libraryman85 <libraryman85@gmail.com>
2018-03-06 12:35:41 -0500

    learnign dunder

--- beal_katas/dunder_score_methods.py
+++ beal_katas/dunder_score_methods.py
@@ -6,9 +6,19 @@
 # __init__ the method that is run when a new instance is created
 
 
-class Adventurer
-    def __init__(self):
-        pass
+class Adventurer:
+    def __init__(self, name):
+        self.name = name
 
+    def __eq__(self, other):
+        return self.name == other.name
 
-sara = Adventurer()
+    def __str__(self):
+        return self.name + ' is an awesome person'
+
+sara1 = Adventurer('sara')
+sara2 = Adventurer('sara')
+
+print(sara1 == sara2)
+print(sara1)
+print(sara2)
